7bd093ee3afa2e01b565d36b7bfaf93ef6d65685
Scott <soltis13@gmail.com>
2018-06-10 10:48:47 -0500

    sat work

--- Homework_5/TriviaGame/assets/javascript/app.js
+++ Homework_5/TriviaGame/assets/javascript/app.js
@@ -1,63 +1,132 @@
 // Game Object
 triviaObj = {
 
+    //Array of Questions  https://quizlet.com/3017165/sit104-flash-cards/
+    questions: [{
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: ["javascript", "js", "scripting", "script"],
+        correctAnswer: "script",
 
-    //Array of Questions
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        answers: ["script href=xxx.js", "script src=xxx.js", "script name=xxx.js", "script img=xxx.js"],
+        correctAnswer: "script src=xxx.js",
+
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: ["alert('Hello World')", "alertBox('Hello World')", "msg('Hello World')", "msgBox('Hello World')"],
+        correctAnswer: "alert('Hello World')",
+
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        answers: ["if i = 5", "if i = 5 then", "if i == 5 then", "if (i == 5)"],
+        correctAnswer: "if (i == 5)",
+
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        answers: ["'This is a comment", "//This is a comment", "!--This is a comment--", "This is a comment"],
+        correctAnswer: "//This is a comment",
+
+    },
+    ],
 
     //Define Varriables
         //correct guesses
+        correctGuesses: 0,
         //incorrect guesses
+        incorrectGuesses: 0,
         //unanswered guesses
         unAnsweredGuesses: 0,
         //timerCounter
+        timerCount: 10,
         //currentQuestion array
+        currentQuestion: [],
         //currentAnswer string
+        currentAnswer: [],
         //UserGuess string
-        clockRunning = false,
-        intervalId = "",
+        userGuess: "",
+
+        clockRunning: false,
+
+        openQuestion: false,
+
+        currentTime: 11,
+
+        questionCounter: 0,
+
+ 
 
 
     // game functions
     
         //Load Start button
+        pageLoad: {
+
+            start: function() {
+            b = $('<button>'),
+            b.addClass('waves-effect waves-light btn-lg text-center startButton')
+            b.html('START!')
+        
+            $('#start').append(b);
+            },
+
+            next: function(){
+                b = $('<button>'),
+                b.addClass('waves-effect waves-light btn-lg text-center startButton')
+                b.html('NEXT')
+            
+                $('#next').append(b);
+            }
+        },
 
         //timer
         stopwatch: {
+
+            intervalId: "",
+
+            time: 10,
+                            
+
             //reset counter
             reset: function() {
 
-                stopwatch.time = 0;
-                stopwatch.lap = 1;
-            
+                triviaObj.stopwatch.time = 10;
+                triviaObj.openQuestion = true;
+
+                            
                 //  TODO: Change the "display" div to "00:00."
-                $("#timer").html("Time Remaining: 00")
+                $("#timer").html('<h2>'+"Time Remaining: 10"+'<h2>')
         
             },
             //start counter
             start: function() {
 
-                if (!clockRunning) {
-                  intervalId = setInterval(stopwatch.count, 10);
-                  clockRunning = true;
+                if (triviaObj.clockRunning === false) {
+                  triviaObj.stopwatch.intervalId = setInterval(triviaObj.stopwatch.count, 1000);
+                  triviaObj.clockRunning = true;
                 }
             },
             //stop counter
             stop: function() {
                 
-                clearInterval(intervalId);
-                clockRunning = false;
+                clearInterval(triviaObj.stopwatch.intervalId);
+                triviaObj.clockRunning = false;
             },
             //decrease counter
             count: function() {
-
+                console.log(triviaObj.stopwatch.time)
                 //  TODO: increment time by 1, remember we cant use "this" here.
-                  stopwatch.time++
+                  triviaObj.stopwatch.time--;
                 //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
                 //        and save the result in a variable.
-                var currentTime = stopwatch.timeConverter(stopwatch.time)
+                currentTime = triviaObj.stopwatch.timeConverter(triviaObj.stopwatch.time);
             
                 //  TODO: Use the variable you just created to show the converted time in the "display" div.
-                $("#timer").html("Time Remaining: " + currentTime)
+                $("#timer").html('<h2>' + "Time Remaining: " + currentTime + '<h2>');
             },
         
             //timer coverter
@@ -72,40 +141,45 @@ triviaObj = {
                 }
             
                 if (minutes === 0) {
-                  minutes = "00";
+                  minutes = "";
                 }
             
                 else if (minutes < 10) {
-                  minutes = "0" + minutes;
+                  minutes = "" + minutes;
                 }
             
-                return minutes + ":" + seconds;
+                return minutes + "" + seconds;
             },
         },
 
         //load question from array
-            //for i in array 
+        questionLoad: function(i) {
+
             // pull question infomation
-            // set question equal to question
+            triviaObj.currentQuestion = triviaObj.questions[i]
+            console.log(triviaObj.currentQuestion.answers.length)
+            console.log(triviaObj.currentQuestion.answers)
             // set answer equal to answer
+            triviaObj.currentAnswer =  triviaObj.currentQuestion[1]
+            console.log(triviaObj.currentAnswer)
             // push infomration to html
+        },
+
 
         //empty html divs
             //set all div equal to blank
-            emptyDivs: function() {
-                $('#question').empty();
-                $('#answer').empty();
-                $('#correctAnswer').empty();
-                $('#timer').empty();
-                $('#start').empty();
-            },
+        emptyDivs: function() {
+            $('#question').empty();
+            $('#answer').empty();
+            $('#correctAnswer').empty();
+        },
 
         //time is up 
             //add to unanswered gueses string
-            timesUp: function() {
-                $('#timer').attr('style', 'font-size: 40px;').html('Times UP!');
-                unAnsweredGuesses++;
-            },
+        timesUp: function() {
+            $('#timer').attr('style', 'font-size: 40px;').html('Times UP!');
+            unAnsweredGuesses++;
+        },
 
         //incorrect guess
             //add to incorrect guesses string
@@ -121,40 +195,92 @@ triviaObj = {
 
     
 };
-//Game Logic
+//Game Logic //triviaObj.
 
     //document ready
+    $(document).ready(function() {    
 
         //Load page and watch for start button press
+        triviaObj.pageLoad.start()
 
-        //for numnber of questions in array
+        
+        $("#start").on('click', function(event){
+            $(this).hide();
+            triviaObj.stopwatch.reset()
+ 
+        console.log(triviaObj.clockRunning)
+        console.log(triviaObj.openQuestion)
+
+        //for i in array ,numnber of questions in array
+        if(triviaObj.openQuestion === true){
+            console.log(triviaObj.clockRunning)
+
+        
+            //load next button
+            triviaObj.pageLoad.next()
+
+            
+
+
+            triviaObj.stopwatch.start()
+            console.log(triviaObj.currentTime)
+     
+            
+
+            
 
             // clear div of content
+            triviaObj.emptyDivs();
 
-            //load question 
+            //load question
+            triviaObj.questionLoad(triviaObj.questionCounter);
+           
 
-            //call start timer
 
-            //watch for on click of next
+            if(triviaObj.currentTime > 0) {
+                //call start timer
+    
+                //watch for on click of next
+                $("#next").on('click', function(event){
+                    triviaObj.stopwatch.stop()
+
+                    triviaObj.questionCounter++;
+
+                    if(triviaObj.userGuess === triviaObj.currentAnswer){
+                        triviaObj.correctAnswer++;
+                    }
+                    else if(triviaObj.userGuess !== triviaObj.currentAnswer){
+                        triviaObj.incorrectGuesses++;
+                    }
+                    
+                })
+
                 
+                console.log("Times up")
                 //stop timer
-
-                //look for user guess
-
-                //clear divs of content
-
-                //add to question answered (right or wrong or unanswered)
-
-                //load next question
-            
-            //watch for timer = 0
-
-                //stop timer
+                //triviaObj.stopwatch.stop()
 
                 //clear dives of content
 
                 //add to question unanswered
+                //triviaObj.unAnsweredGuesses++
 
                 //load next question
+            }  
+
+
+            
+            
+
+            
+
+
+      
+        }
+        })
+
+
+
         
         //load end page and print out 
+    });
