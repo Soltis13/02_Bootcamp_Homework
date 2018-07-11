e8460c13690f74b9ed8e7b76bff5a49377454271
Scott <Soltis13@gmail.com>
2018-07-11 13:49:13 -0500

    home work

--- Homework_8/assets/javascript/scripts.js
+++ Homework_8/assets/javascript/scripts.js
@@ -1,199 +1,45 @@
 
-(function ($) {
 
-    "use strict";
-	
-	
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
 
-	// LINE PROGRESS BAR
-	enableLineProgress();
-	
-	// RADIAL PROGRESS BAR
-	enableRadialProgress();
-	
-	// ACCORDIAN
-	panelAccordian();
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
-	$(window).on('load', function(){
-		
-		// ISOTOPE PORTFOLIO WITH FILTER
-		if(isExists('.portfolioContainer')){
-			var $container = $('.portfolioContainer');
-			$container.isotope({
-				filter: '*',
-				animationOptions: {
-					duration: 750,
-					easing: 'linear',
-					queue: false
-				}
-			});
-		 
-			$('.portfolioFilter a').click(function(){
-				$('.portfolioFilter .current').removeClass('current');
-				$(this).addClass('current');
-		 
-				var selector = $(this).attr('data-filter');
-				$container.isotope({
-					filter: selector,
-					animationOptions: {
-						duration: 750,
-						easing: 'linear',
-						queue: false
-					}
-				 });
-				 return false;
-			}); 
-		}
-	
-	});
-	
-	
-	$('a[href="#"]').on('click', function(event){
-		return;
-	});
-	
-	
-	if ( $.isFunction($.fn.fluidbox) ) {
-		$('a').fluidbox();
-	}
-	
-	var countCounterUp = 0;
-	
-	var a = 0 ;
-	
-	countCounterUp = enableCounterUp(countCounterUp);
-	
-	$(window).on('scroll', function(){
-		
-		countCounterUp = enableCounterUp(countCounterUp);
-	
-	});
-	
-	
-})(jQuery);
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
 
-function panelAccordian(){
-	
-	var panelTitle = $('.panel-title');
-	panelTitle.on('click', function(){
-		$('.panel-title').removeClass('active');
-		$(this).toggleClass('active');
-		
-	});
-	
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
 }
 
-function enableRadialProgress(){
-	
-	$(".radial-progress").each(function(){
-		var $this = $(this),
-			progPercent = $this.data('prog-percent');
-			
-		var bar = new ProgressBar.Circle(this, {
-			color: '#aaa',
-			strokeWidth: 3,
-			trailWidth: 1,
-			easing: 'easeInOut',
-			duration: 1400,
-			text: {
-				
-			},
-			from: { color: '#aaa', width: 1 },
-			to: { color: '#FEAE01', width: 3 },
-			// Set default step function for all animate calls
-			step: function(state, circle) {
-				circle.path.setAttribute('stroke', state.color);
-				circle.path.setAttribute('stroke-width', state.width);
-
-				var value = Math.round(circle.value() * 100);
-				if (value === 0) {
-					circle.setText('');
-				} else {
-					circle.setText(value);
-				}
-
-			}
-		});
-		
-		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
-		},{offset: "90%"})
-		
-	});
-}
-
-function enableLineProgress(){
-	
-	$(".line-progress").each(function(){
-		var $this = $(this),
-			progPercent = $this.data('prog-percent');
-			
-		var bar = new ProgressBar.Line(this, {
-			strokeWidth: 1,
-			easing: 'easeInOut',
-			duration: 1400,
-			color: '#FEAE01',
-			trailColor: '#eee',
-			trailWidth: 1,
-			svgStyle: {width: '100%', height: '100%'},
-			text: {
-				style: {
-					
-				},
-			},
-			from: {color: '#FFEA82'},
-			to: {color: '#ED6A5A'},
-			step: (state, bar) => {
-				bar.setText(Math.round(bar.value() * 100) + ' %');
-			}
-		});
-		
-		$(this).waypoint(function(){
-		   bar.animate(progPercent);  
-		},{offset: "90%"})
-		
-	});
-}
-
-function enableCounterUp(a){
-	
-	var counterElement;
-	
-	if(isExists('#counter')){ counterElement = $('#counter'); }
-	else{ return; }
-		
-	var oTop = $('#counter').offset().top - window.innerHeight;
-	if (a == 0 && $(window).scrollTop() > oTop) {
-		$('.counter-value').each(function() {
-			var $this = $(this),
-				countDuration = $this.data('duration'),
-				countTo = $this.attr('data-count');
-			$({
-				countNum: $this.text()
-			}).animate({
-				countNum: countTo
-			},{
-
-				duration: countDuration,
-				easing: 'swing',
-				step: function() {
-					$this.text(Math.floor(this.countNum));
-				},
-				complete: function() {
-					$this.text(this.countNum);
-				}
-
-			});
-		});
-		a = 1;
-	}
-
-	return a;
-}
-
-function isExists(elem){
-	if ($(elem).length > 0) { 
-		return true;
-	}
-	return false;
-}
+$('.smooth-goto').on('click', function() {  
+    $('html, body').animate({scrollTop: $(this.hash).offset().top - 50}, 1000);
+    return false;
+});
