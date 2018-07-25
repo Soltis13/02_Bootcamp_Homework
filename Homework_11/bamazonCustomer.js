c1788671d307b1bb30dd1b33b83d8599be6ce1c1
Scott <Soltis13@gmail.com>
2018-07-25 18:38:15 -0500

    added manager to home 11

--- Homework_11/bamazonCustomer.js
+++ Homework_11/bamazonCustomer.js
@@ -1,3 +1,14 @@
+// -----------------
+//Application part 1
+//00 - pre connection and varraibles
+//01 - connect to sql
+//02 - read products from sql database - function readProducts.
+//03 - prompt the user for input to buy from database
+//04 - Request from the database to update inventory
+//05 - start application
+
+// -----------------
+//00 - pre connection and varraibles
 //require npm libraries
 var mysql = require("mysql");
 var inquirer = require('inquirer');
@@ -22,49 +33,63 @@ var connection = mysql.createConnection({
     database: "bamazon"
   });
 
-//Application part 1
 
-//display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// -----------------
+//01 - Connect to SQL.  
+//Display all of the items available for sale. Include the ids, names, and prices of products for sale.
 function connect(){
   connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    console.log("\n------------------------------------------------------------\n")
+    console.log("-----------------------BAMAZON STORE------------------------")
+    console.log("\n------------------------------------------------------------\n")
+
+    console.log("\n------------------------------------------------------------\n")
+    console.log("      Now connected to database "+connection.config.database+" as User " + connection.threadId);
+    console.log("\n------------------------------------------------------------\n")
   });
   readProducts()
 }
 
+// -----------------
+//02 - get information from database
 function readProducts() {
-  console.log("Selecting all products...\n");
+  console.log("\n\nSelecting all products...\n");
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     let table = new Tablefy()
     var storeInfo = res
     // Log all results of the SELECT statement
-   
-    console.log("\n\nThe following Items are now for sale in the its-not-amazon store.\n\n")
-    console.log("------------------------------------------------------------------------")
+    console.log("\n------------------------------------------------------------\n")
+    console.log("            The following are now for sale.")
+    console.log("\n------------------------------------------------------------\n\n")
     table.draw(res)
-    // for(var i=0;i<res.length;i++){
-    //   console.log("\n   ITEM:       "+res[i].item_id)
-    //   productIds.push(res[i].item_id)
-    //   console.log("\n   Product:    "+res[i].product_name)
-    //   console.log("\n   Department: "+res[i].department_name)
-    //   console.log("\n   Price($):   "+res[i].price)
-    //   console.log("\n   Qty:        "+res[i].stock_quantity)
-    //   console.log("\n----------------------------------")
-      
-    // }
+
+    //alternate method of outputting datbase
+      // for(var i=0;i<res.length;i++){
+      //   console.log("\n   ITEM:       "+res[i].item_id)
+      //   productIds.push(res[i].item_id)
+      //   console.log("\n   Product:    "+res[i].product_name)
+      //   console.log("\n   Department: "+res[i].department_name)
+      //   console.log("\n   Price($):   "+res[i].price)
+      //   console.log("\n   Qty:        "+res[i].stock_quantity)
+      //   console.log("\n----------------------------------")
+      // }
+
     console.log("\n\n\n")
+    //call function to prompt user for input
     UserEntry(productIds, storeInfo)
   });
 }
 
+// -----------------
+//03 - call function to prompt user for input, buy from database.
 function UserEntry(productIds, storeInfo){
-  //console.log(productIds)
-  //console.log(productIds.length)
-//prompt user with two messages
 
+  //pre defined regex
   var re = /([0-9])/;
+
+  //prompt user with two messages  
   inquirer.prompt([
     //1. The first should ask them the ID of the product they would like to buy.
     {
@@ -72,59 +97,42 @@ function UserEntry(productIds, storeInfo){
       name: "Ids",
       message: "What product (Product ID) would you like to buy?"     
 
-    },
-    
+    },    
     //2. The second message should ask how many units of the product they would like to buy.
     {
       type: "input",
       name: "units",
       message: "How many units do you wish to purchase?",
-      validate: function validateUserUnits(name, Ids){
-        if(!re.exec(name)){
-          console.log("Not a valid entry.")
+      validate: function validateUserUnits(units, Ids){
+        //console.log(units)
+        //console.log(storeInfo[Ids.Ids-1])
+        //console.log(Ids)
+        if(!re.exec(units)){
+          console.log("\n\n  Not a valid entry.\n\n")
           return false
-        }else if(name.units>storeInfo[Ids-1].stock_quantity){
-          console.log("Error. Not enough inventory.  Only "+storeInfo[userRequest.Ids-1].stock_quantity+" available.") 
+        }//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+        else if(units>storeInfo[Ids.Ids-1].stock_quantity){
+          //console.log(storeInfo)          
+          console.log("\n\n  Error. Not enough inventory.  Only "+storeInfo[Ids.Ids-1].stock_quantity+" available.\n\n") 
           return false
         }
         else{return true}
       }
     }
-
   ]).then(answers =>{
     //console.log(answers)
+
     //go to function, check store for purchase
-  Purchase(productIds, storeInfo, answers)
+    UpdateProduct(productIds, storeInfo, answers)
   })
-
- 
 }
 
-function Purchase(productIds, storeInfo, userRequest){
-//Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-// console.log("answers"+JSON.stringify(userRequest))
-// console.log("storeInfo"+JSON.stringify(storeInfo))
-// console.log("ProductID"+JSON.stringify(productIds))
-// console.log(userRequest.units)
-// console.log(userRequest.Ids)
-// console.log(storeInfo[userRequest.Ids-1])
-
-
-//If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
-if(userRequest.units>storeInfo[userRequest.Ids-1].stock_quantity){
-  console.log("Error. Not enough inventory.  Only "+storeInfo[userRequest.Ids-1].stock_quantity+" available.") 
-  UserEntry()
-}else{
-  UpdateProduct(productIds, storeInfo, userRequest)
-
-}
-
-}
-
-
+// -----------------
+//04 - Request from the database to update inventory
 function UpdateProduct(productIds, storeInfo, userRequest) {
-  console.log("Purchasing "+userRequest.units+" "+storeInfo[userRequest.Ids-1].product_name+"\n");
+  console.log("\n------------------------------------------------------------\n")
+  console.log("    Purchasing "+userRequest.units+" "+storeInfo[userRequest.Ids-1].product_name);
+  console.log("\n------------------------------------------------------------\n")
   
   var updatedQty = storeInfo[userRequest.Ids-1].stock_quantity - userRequest.units
   //console.log("Value to reduce to: "+updatedQty)
@@ -144,28 +152,27 @@ function UpdateProduct(productIds, storeInfo, userRequest) {
     function(err, res) {
       // logs the actual query being run
       if (err) throw err;
-      //However, if your store does have enough of the product, you should fulfill the customer's order.
-
-      //This means updating the SQL database to reflect the remaining quantity.
+      //However, if your store does have enough of the product, you should fulfill the customer's order.  This means updating the SQL database to reflect the remaining quantity.
 
       //Once the update goes through, show the customer the total cost of their purchase.
-
-      console.log("Update Successful.  You purchased "+userRequest.units+" "+storeInfo[userRequest.Ids-1].product_name+"\n")
+      console.log("\n------------------------------------------------------------\n")
+      console.log("    Success, You purchased "+userRequest.units+" "+storeInfo[userRequest.Ids-1].product_name)
+      console.log("\n------------------------------------------------------------\n")
       //console.log(JSON.stringify(res) + " products updated!\n");
       var cost = userRequest.units * storeInfo[userRequest.Ids-1].price
-      console.log("Your total cost is: "+cost)
-         
+      console.log("\n------------------------------------------------------------\n")
+      console.log("    Your total cost is: $"+cost)   
+      console.log("\n------------------------------------------------------------\n\n\n")
     },
-   
   );
+  //end sql connection
   connection.end()
-
-  //console.log(query.sql);
 }
 
+// -----------------
+//05 - start application
 function main(){
   connect()
 }
-
 main()
 
