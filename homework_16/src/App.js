9b414a6a5c7d7157efb38bee6cf9abd550141be9
Scott <Soltis13@gmail.com>
2018-09-18 15:30:52 -0500

    started clicky game homework

---
+++ homework_16/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Clicky Game</h1>
+          <h1 className="App-title"
+            //onChange from inputChangedHandler
+            //change the value of score
+            //an h1 of the userinput state
+            //component to collect teh score and high score
+            //{output}
+          >You guessed</h1>
+
+          <input type="text" 
+            //onChange from inputChangedHandler
+            //change the value of score
+            //an h1 of the userinput state
+            //component to collect teh score and high score
+            //{output}
+          />
+        </header>
+
+        <body className="App-body">
+          <h1>Clicky Game!</h1>
+          <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+          <div //image div
+          />
+        </body>
+
+        <footer className="App-footer">
+          <p>Clicky Game</p>
+          <p //link to bootcamp code
+          />
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
